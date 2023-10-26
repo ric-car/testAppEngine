e0b6e6a2983718f20c023926a636bbaf60d74a5a
Riccardo Carraro <rcar@bendingspoons.com>
2023-10-26 12:47:48 +0200

    feat: app api

--- app/main.py
+++ app/main.py
@@ -4,16 +4,16 @@ Primary FastPI ASGI application
 """
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
-
+import google.appengine.api
 from app.api.endpoints import api_routes
 
 
 def create_app():
     # Initialize FastAPI app
-    app = FastAPI() 
+    app1 = FastAPI()
 
     # Enable CORS via middleware
-    app.add_middleware(
+    app1.add_middleware(
         CORSMiddleware,
         allow_credentials=True,
         allow_headers=['*'],
@@ -21,9 +21,13 @@ def create_app():
         allow_origins=['*'],
     )
 
-    app.include_router(api_routes)
+    app1.include_router(api_routes)
 
-    return app
+    return app1
 
-
+def app(environ, start_response):
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+    yield b'Hello world!\n'
+ 
+app = google.appengine.api.wrap_wsgi_app(app)
 application = create_app()
